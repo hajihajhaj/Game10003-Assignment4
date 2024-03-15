52a11d6a41459fbf6a4cb7487520d6f47b810da0
Hajihajhaj <000915724@mohawkcollege.ca>
2024-03-14 21:24:07 -0500

    initial project setup

---
+++ Game10003-a4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Game10003_a4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
